182e3f1dabdf72f6e7709f9066fdbb6be8955fa9
natchezl <natchez.l234@gmail.com>
2014-11-22 15:44:47 +0700

    no message

--- application/models/product_m.php
+++ application/models/product_m.php
@@ -1,28 +1,114 @@
 <?php
-Class Product_m extends CI_Model
+Class product_m extends CI_Model
 {
-	function search($data){
-		$query = $this->db->get_where('Product',array('name'=>$data),40);
+	function search($searchWord){
+		// $query = $this->db->get_where('Product',array('name'=>$searchWord),40);
+		$query = $this->db->query("Select * from product where Name like '%$searchWord%'");
 		return $query;
 	}
-	function allProduct(){
-		$this->db->limit(10);
+	function allProduct($page_num){
+		$min = ($page_num-1)*10+1;
+		$this->db->limit(10,0);
 		$query = $this->db->get('Product');
 		return $query;
 	}
 	function getDetail($id){
-		$query = $this->db->get_where('Product',array('id'=>$id));
+		$query = $this->db->get_where('Product',array('product_id'=>$id));
 		if($query->num_rows() == 1)
 		{
 			return $query->row();
 		}
 		else
 		{
-			return false;
+			return null;
 		}		
 	}
-	function editProduct($id, $name, $image,$brand, $model, $price,$additional_info,$capacity,$size,$property,$defect,$quality,$payment,$return_product,$return_fee,$packaging,$delivery_fee,$delevery_confirmation,$tax,$seller_id,$datetime){
-		$data = array(
+	function newDirectProduct($name,$image,$brand,$model,$price,$additional_info,$capacity,
+							$size,$property,$defect,$quality,$payment,$return_product,$return_fee,
+							$packaging,$delivery_fee,$delivery_confirmation,$tax,$quantity,$user_id){
+		$data1 = array(
+			// 'Product_ID' => $id, 
+			'Name' => $name, 
+			'Image'=> $image,
+			'Brand' => $brand, 
+			'Model' => $model, 
+			'Price' => $price,
+			'Additional_Info'=> $additional_info,
+			'Capacity'=> $capacity,
+			'Size'=> $size,
+			'Property' => $property,
+			'Defect' => $defect,
+			'Quality' => $quality,
+			'Payment' => $payment,
+			'Return_Product' => $return_product,
+			'Return_Fee' => $return_fee,
+			'Packaging' => $packaging,
+			'Delivery_Fee' => $delivery_fee,
+			'Delivery_Confirmation' => $delivery_confirmation,
+			'Tax' => $tax,
+			'Seller_ID' => $user_id
+			);
+			$this->db->trans_start();
+			$this->db->insert('product', $data1);
+			$insert_id = $this->db->insert_id();
+		$data2 = array(
+			'Product_ID' => $insert_id,
+			'Quantity' => $quantity
+		); 
+			$this->db->insert('product_direct', $data2);
+			$this->db->trans_complete();
+			return $insert_id;
+	}
+	
+	function newBiddingProduct($name,$image,$brand,$model,$price,$additional_info,$capacity,
+		$size,$property,$defect,$quality,$payment,$return_product,$return_fee,$packaging,$delivery_fee,
+		$delivery_confirmation,$tax,$end_datetime,
+		$status,$current_price,$current_max_bid,$current_win_cust_id,$bid_increment,$user_id){
+
+		$data1 = array(
+			'Name' => $name, 
+			'Image'=> $image,
+			'Brand' => $brand, 
+			'Model' => $model, 
+			'Price' => $price,
+			'Additional_Info'=> $additional_info,
+			'Capacity'=> $capacity,
+			'Size'=> $size,
+			'Property' => $property,
+			'Defect' => $defect,
+			'Quality' => $quality,
+			'Payment' => $payment,
+			'Return_Product' => $return_product,
+			'Return_Fee' => $return_fee,
+			'Packaging' => $packaging,
+			'Delivery_Fee' => $delivery_fee,
+			'Delivery_Confirmation' => $delivery_confirmation,
+			'Tax' => $tax,
+			'Seller_ID' => $user_id
+			);
+		$this->db->trans_start();
+		$this->db->insert('product', $data1);
+		$insert_id = $this->db->insert_id();
+
+		$data2 = array(
+			'Product_ID' => $insert_id,
+			'Status' => $status,
+			'Current_Price' => $current_price,
+			'Current_Max_Bid' => $current_max_bid, 
+			'Bid_Increment' => $bid_increment,
+			'End_Date' => $end_datetime
+			);
+		$this->db->insert('product_bid', $data2);
+		$this->db->trans_complete();
+		return $insert_id;
+
+	}
+
+	function editBiddingProduct($id, $name, $image,$brand, $model, $price,$additional_info,
+								$capacity,$size,$property,$defect,$quality,$payment,$return_product,$return_fee,
+								$packaging,$delivery_fee,$delevery_confirmation,$tax,$seller_id,
+								$status,$current_price,$current_max_bid, $bid_increment,$end_date){
+		$data1 = array(
 			//'Product_ID' => $id, 
 			'Name' => $name, 
 			'Image'=> $image,
@@ -40,23 +126,109 @@ Class Product_m extends CI_Model
 			'Return_Fee' => $return_fee,
 			'Packaging' => $packaging,
 			'Delivery_Fee' => $delivery_fee,
-			'Delevery_Confirmation' => $delevery_confirmation,
+			'Delivery_Confirmation' => $delevery_confirmation,
 			'Tax' => $tax,
-			'Seller' => $seller_id,
-			'Datetime' => $datetime	
+			'Seller_ID' => $seller_id
+			);
+			$this->db->trans_start();
+			$this->db->where('Product_ID', $id);
+			$this->db->update('product', $data1);
+			$product_r = ($this->db->affected_rows() > 0);
+			// echo "product ".$product_r."<br>";
+			// if ($this->db->affected_rows() <= 0) return "false";
+
+		$data2 = array(
+			'Status' =>$status,
+			'Current_Price' => $current_price,
+			'Current_Max_Bid' => $current_max_bid, 
+			'Bid_Increment' => $bid_increment,
+			'End_Date' => $end_date
 			);
 			$this->db->where('Product_ID', $id);
-			$this->db->update('product', $data); 
-			return true;
+			$this->db->update('product_bid', $data2) ."<br>";
+			$product_bid_r = ($this->db->affected_rows() > 0);
+			// echo "<br> product_bid " .$product_bid_r. "<br>";
+			$this->db->trans_complete();
+			if ($product_bid_r or $product_r) return "true";
+			else return "false";
 	}
-	function deleteProduct(){
-		$this->db->where('Product_ID', $id);
+	function editDirectProduct($id, $name, $image,$brand, $model, $price,$additional_info,
+								$capacity,$size,$property,$defect,$quality,$payment,$return_product,$return_fee,
+								$packaging,$delivery_fee,$delivery_confirmation,$tax,$seller_id,
+								$quantity){
+		$data1 = array(
+			//'Product_ID' => $id, 
+			'Name' => $name, 
+			'Image'=> $image,
+			'Brand' => $brand, 
+			'Model' => $model, 
+			'Price' => $price,
+			'Additional_Info'=> $additional_info,
+			'Capacity'=> $capacity,
+			'Size'=> $size,
+			'Property' => $property,
+			'Defect' => $defect,
+			'Quality' => $quality,
+			'Payment' => $payment,
+			'Return_Product' => $return_product,
+			'Return_Fee' => $return_fee,
+			'Packaging' => $packaging,
+			'Delivery_Fee' => $delivery_fee,
+			'Delivery_Confirmation' => $delivery_confirmation,
+			'Tax' => $tax,
+			'Seller_ID' => $seller_id,
+			);
+			$this->db->trans_start();
+			$this->db->where('Product_ID', $id);
+			$this->db->update('product', $data1);
+		$data2 = array(
+			'Quantity' => $quantity
+			); 
+			$this->db->where('Product_ID', $id);
+			$this->db->update('product_direct', $data2);
+			$product_direct_r = ($this->db->affected_rows() > 0);
+			$this->db->trans_complete();
+			if ($product_direct_r or $product_r) return "true";
+			else return "false";
+	}
+	function deleteProduct($product_id){
+		$this->db->trans_start();
+		$this->db->where('Product_ID', $product_id);
 		$this->db->delete('product');
-		return true;
-	}
-	//function newDirectProduct();
-	
-	//function newBiddingProduct();
+		$result = ($this->db->affected_rows() > 0);
 
+		$this->db->where('Product_ID', $product_id);
+		$this->db->delete('product_direct');
+		$result2 = ($this->db->affected_rows() > 0);
+		
+		$this->db->where('Product_ID', $product_id);
+		$this->db->delete('product_bid');
+		$result2 = $result2 or ($this->db->affected_rows() > 0);
+
+		$this->db->trans_complete();
+
+		if ($this->product_m->checkExistProduct($product_id) == "false") return "true";
+		else return "false";
+	}
+	function checkExistProduct($product_id){
+		$query = $this->db->get_where('Product',array('product_id'=>$product_id));
+		if($query->num_rows() >0){ 
+			$checkExBidProduct = $this->product_m->checkExistProductBid($product_id);
+			$checkExDirectProduct = $this->product_m->checkExistProductDirect($product_id);
+			if($checkExDirectProduct and $checkExBidProduct) return "true";
+			else return "false";
+		}
+		else return "false";
+	}
+	function checkExistProductBid($product_id){
+		$query = $this->db->get_where('Product_bid',array('product_id'=>$product_id));
+		if($query->num_rows() >0) return true;
+		else return false;
+	}
+	function checkExistProductDirect($product_id){
+		$query = $this->db->get_where('Product_direct',array('product_id'=>$product_id));
+		if($query->num_rows() >0) return true;
+		else return false;
+	}
 }
 ?>
